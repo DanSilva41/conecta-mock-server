b3ca51f94eae06d4e9619d8bbd9a9ec1ca647979
Danilo da Silva Pereira <t0305dnl@prestadorcbmp.com.br>
2021-05-22 08:15:52 -0300

    Add: /categories

--- api/server.js
+++ api/server.js
@@ -3,8 +3,11 @@ const server = jsonServer.create()
 const middlewares = jsonServer.defaults()
 const port = process.env.PORT || 3000
 
+const mainRouter = jsonServer.router('api/mocks/main.json')
+
 server.use(jsonServer.bodyParser)
 server.use(middlewares)
+server.use(mainRouter)
 
 server.listen(port, () => {
     console.log('Conecta - Mock Server is running')
